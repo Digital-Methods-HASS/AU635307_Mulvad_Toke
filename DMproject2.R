5daf2dadb7a9f9d6fc363a7e95feb89883a94021
TokeJoMu <73120367+TokeJoMu@users.noreply.github.com>
2020-12-28 11:43:14 +0100

    Final Project work in RStudio and Text Analysis
    Loading of corpus, pre-processing, NRC analysing

---
+++ DMproject2.R
@@ -0,0 +1,15 @@
+library(nrc)
+??get_sentiment
+#install.packages("textdata")
+library(textdata)
+library(fs)
+library(readr)
+library(tibble)
+#install.packages("rappdirs")
+library(rappdirs)
+
+??get_sentiment
+
+??pander
+
+#install.packages("pander")
